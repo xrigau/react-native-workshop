2cb7d629b16a83573f33d63851fef5b83c4ff433
Xavi Rigau <xavi@novoda.com>
2018-08-07 18:28:39 +0200

    Add progress from 2nd session

--- App.js
+++ App.js
@@ -1,38 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
-
+import { StyleSheet, Text, View, FlatList, SafeAreaView, Switch } from 'react-native';
 
 const todos = [
   {
-    name: "Take the dog out"
+    name: "Take the dog out",
+    checked: false
   },
   {
-    name: "Buy potato chips"
+    name: "Buy potato chips",
+    checked: true
   },
   {
-    name: "Become millionaire"
+    name: "Become millionaire",
+    checked: false
   }
-]
+];
 
-const TodoItem = ({ name }) => (
-  <Text>{name}</Text>
+const TodoItem = ({ name, checked, onTodoChanged }) => (
+  <View style={{ flexDirection: 'row' }}>
+    <Text>{name}</Text>
+    <Switch value={checked} onValueChange={value => onTodoChanged(name, value)} />
+  </View>
 )
 
-const TodoList = ({ items }) => (
-  <FlatList
+const TodoList = ({ items, onTodoChanged }) => {
+  const renderItem = ({ item }) => (<TodoItem name={item.name} checked={item.checked} onTodoChanged={(name, value) => onTodoChanged(name, value)} />);
+
+  return <FlatList
     data={items}
-    renderItem={({ item }) => (<TodoItem name={item.name} />)}
+    renderItem={renderItem}
     keyExtractor={(item) => item.name}
-    />
-)
+  />
+};
 
 export default class App extends React.Component {
 
+  state = {
+    todos: todos
+  };
+
   render() {
+    const onTodoChanged = (name, checked) => {
+      this.setState(oldState => {
+        const newTodos = oldState.todos.map(todo => {
+          if (todo.name !== name) return todo;
+          return { ...todo, checked };
+        });
+        return { todos: newTodos };
+      });
+    };
+
     return (
       <View style={styles.container}>
         <SafeAreaView>
-          <TodoList items={todos} />
+          <TodoList items={this.state.todos} onTodoChanged={(name, value) => onTodoChanged(name, value)} />
         </SafeAreaView>
       </View>
     );
